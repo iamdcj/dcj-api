3d3088d778a84aa1468dce20896388788f136ed7
David Christian Jones <david@dcj.codes>
2021-02-27 18:33:30 -0500

    fake apiCall

--- src/handle-request/index.ts
+++ src/handle-request/index.ts
@@ -1,20 +1,40 @@
 import { Response } from "express";
 
-const handleRequest = (path: String, res: Response) => {
+async function handleRequest(path: String, res: Response) {
 
-  res.send(`
+  const apiCall = () => {
+    return new Promise<{ title: String, content?: String }>((resolve) => {
+      setTimeout(function () {
+        const data = {
+          title: "David Christian Jones",
+          content: "Software Engineer"
+        }
+
+        resolve(data);
+      }, 100);
+    });
+  }
+
+  try {
+    const response = await apiCall();
+
+    res.send(`
     <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Page: ${path}</title>
+            <title>${response.title}</title>
         </head>
         <body>
-            <div>${path}</div>
+            <h1>${response.title}</h1>
+            <p>${response.content}</>
         </body>
     </html>
-    `)
+  `);
+  } catch (error) {
+    console.error(error);
+  }
 
 }
 
